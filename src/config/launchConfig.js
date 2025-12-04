a9abb3eeae23d512e341fa9c04564ae355f15ba3
Luigi Schiavone <schiavone_lu@hotmail.it>
2025-12-04 18:54:18 +0100

    prova

--- src/config/launchConfig.js
+++ src/config/launchConfig.js
@@ -1,7 +1,7 @@
 // src/config/launchConfig.js
 
 // 🔁 DATA DI LANCIO REALE
-export const LAUNCH_DATE = new Date("2025-12-13T20:00:00+01:00");
+export const LAUNCH_DATE = new Date("2025-12-05T20:00:00+01:00");
 // formato: new Date("YYYY-MM-DDTHH:MM:SS+01:00")
 
 // 🛠 INIZIO MODALITÀ MANUTENZIONE (quando metti online su Ionos)
